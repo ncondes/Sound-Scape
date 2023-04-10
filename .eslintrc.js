327db5cdc1d03e87800bd1b8efa2589d5d55d580
nicolas conde <ncondes@unal.edu.co>
2023-04-09 21:55:35 -0500

    initial configuration

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+   root: true,
+   extends: ['eslint:recommended'],
+   parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+   }
+}
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+   return (
+      <div className="flex h-screen w-screen justify-center items-center">
+         <h1 className="text-5xl font-bold mr-5">Sound Scape</h1>
+         <i class="fa-solid fa-code fa-xl mt-3"></i>
+      </div>
+   )
+}
+
+export default App
