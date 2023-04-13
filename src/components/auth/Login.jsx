7c44ee31db6f71fc6bcfa5d589b16a923e119776
nzone56 <jpereirap@unal.edu.co>
2023-04-13 15:12:14 -0500

    Corrections of fussy senior done

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react'
 
 export const Login = () => {
 
@@ -17,25 +16,25 @@ export const Login = () => {
    return (
       <form onSubmit={onFormSubmit}>
          <div className="flex flex-col my-1">
-            <label className="font-medium mt-2 mb-1"> Email </label>
+            <label className="font-medium mt-2 mb-1">Email</label>
             <input
                className="my-1 border-gray-200 border-2 p-2 rounded-md placeholder:font-medium"
                type="email"
                placeholder="Enter Email"
                name='email'
                onChange={handleInputChange}
-            ></input>
-            <label className="font-medium mt-2 mb-1"> Password </label>
+            />
+            <label className="font-medium mt-2 mb-1">Password</label>
             <input
                className="my-1 border-gray-200 border-2 p-2 rounded-md placeholder:font-medium"
                type="password"
                placeholder="Password"
                name='password'
                onChange={handleInputChange}
-            ></input>
+            />
          </div>
-         <div className="submit mt-2 text-center">
-            <button className="mt-1 bg-purple-600 text-white border-2 p-2 rounded-md font-medium w-full">
+         <div className="mt-2 text-center">
+            <button type="submit" className="mt-1 bg-purple-600 text-white border-2 p-2 rounded-md font-medium w-full">
                Submit
             </button>
          </div>
