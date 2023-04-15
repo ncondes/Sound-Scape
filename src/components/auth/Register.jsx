7c5e1c654851869f37f355810cce78048777f785
nzone56 <jpereirap@unal.edu.co>
2023-04-14 22:37:27 -0500

    SS012 with SS014, created the register from validation with all tested rules, with a little sneak into SS015

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,18 +1,37 @@
+import { useForm } from 'react-hook-form'
+
+
 export const Register = () => {
+
+   const { register, handleSubmit, formState: { errors } } = useForm();
    const handleInputChange = (event) => {
       // TODO: validate form
    }
-
-   const onFormSubmit = (event) => {
-      event.preventDefault()
-      const formData = new FormData(event.target)
-      const data = Object.fromEntries(formData.entries())
-      data.terms = event.target.terms.checked
+     const validatePassword = (value) => {
+        if (value === 'password') {
+           return "Password cannot be 'password'"
+        }
+     }
+    const validateConfirmPassword = (value, values) => {
+       if (value !== values.password) {
+          return 'Passwords do not match'
+       }
+    }
+    const validateCountry = (value) => {
+       if (value === 'Argentina') {
+          return 'We currently do not support Argentina'
+       }
+    }
+   const onSubmit = (data) => {
+   //    event.preventDefault()
+   //    const formData = new FormData(event.target)
+   //    const data = Object.fromEntries(formData.entries())
+   //    data.terms = event.target.terms.checked
       console.log(data)
    }
 
    return (
-      <form onSubmit={onFormSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
          <div className="flex flex-col my-1">
             {/* name */}
             <div className="mb-3">
@@ -21,9 +40,14 @@ export const Register = () => {
                   className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded"
                   type="text"
                   placeholder="Enter Name"
-                  name="name"
+                  {...register('name', {
+                     required: 'Name is required',
+                     maxLength: { value: 20, message: 'Name cannot exceed 20 characters' },
+                     minLength: { value: 2, message: 'Name must be at least 2 characters' }
+                  })}
                   onChange={handleInputChange}
                />
+               {errors.name && <p className="text-red-600">{errors.name.message}</p>}
             </div>
             {/* email */}
             <div className="mb-3">
@@ -32,9 +56,16 @@ export const Register = () => {
                   className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded"
                   type="email"
                   placeholder="Enter Email"
-                  name="email"
+                  {...register('email', {
+                     required: 'Email is required',
+                     pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                        message: 'Invalid email address'
+                     }
+                  })}
                   onChange={handleInputChange}
                />
+               {errors.email && <p className="text-red-600">{errors.email.message}</p>}
             </div>
             {/* age */}
             <div className="mb-3">
@@ -43,9 +74,14 @@ export const Register = () => {
                   className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded"
                   type="number"
                   placeholder="Age"
-                  name="age"
+                  {...register('age', {
+                     required: 'Age is required',
+                     min: { value: 18, message: 'You must be at least 18 years old' },
+                     max: { value: 85, message: 'You cannot be older than 85 years old' }
+                  })}
                   onChange={handleInputChange}
                />
+               {errors.age && <p className="text-red-600">{errors.age.message}</p>}
             </div>
             {/* password */}
             <div className="mb-3">
@@ -54,9 +90,21 @@ export const Register = () => {
                   className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded"
                   type="password"
                   placeholder="Password"
-                  name="password"
+                  {...register('password', {
+                     required: 'Password is required',
+                     minLength: {
+                        value: 6,
+                        message: 'Password must be at least 6 characters'
+                     },
+                     maxLength: {
+                        value: 30,
+                        message: 'Password cannot be more than 30 characters'
+                     },
+                     validate: validatePassword
+                  })}
                   onChange={handleInputChange}
                />
+               {errors.password && <p className="text-red-600">{errors.password.message}</p>}
             </div>
             {/* confirm password */}
             <div className="mb-3">
@@ -65,29 +113,56 @@ export const Register = () => {
                   className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded"
                   type="password"
                   placeholder="Confirm Password"
-                  name="confirm-password"
+                  {...register('confirmPassword', {
+                     required: 'Confirm Password is required',
+                     minLength: {
+                        value: 6,
+                        message: 'Confirm Password must be at least 6 characters'
+                     },
+                     maxLength: {
+                        value: 30,
+                        message: 'Confirm Password cannot be more than 30 characters'
+                     },
+                     validate: { value: validateConfirmPassword }
+                  })}
                   onChange={handleInputChange}
                />
+               {errors.confirmPassword && (
+                  <p className="text-red-600">{errors.confirmPassword.message}</p>
+               )}
             </div>
             {/* country */}
             <div className="mb-3">
                <label className="inline-block mb-2">Country</label>
-               <select className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded">
+               <select
+                  {...register('country', {
+                     required: 'Country is required',
+                     validate: validateCountry
+                  })}
+                  className="block w-full border border-gray-300 text-gray-800 py-1.5 px-3 transition duration-500 focus:outline-none focus:border-black rounded"
+               >
                   <option value="USA">USA</option>
                   <option value="Colombia">Colombia</option>
                   <option value="Russia">Russia</option>
+                  <option value="Argentina">Argentina</option>
                </select>
+               {errors.country && <p className="text-red-600">{errors.country.message}</p>}
             </div>
             {/* terms */}
             <div className="mb-3 flex items-center">
                <input
                   className="rounded border border-gray-500 focus:ring-transparent"
                   type="checkbox"
-                  name="terms"
+                  {...register('termsOfService', {
+                     required: 'You must accept the Terms of Service'
+                  })}
                   onChange={handleInputChange}
                />
                <label className="inline-block ml-2">I accept Music's Terms of Service</label>
             </div>
+            {errors.termsOfService && (
+               <p className="text-red-600">{errors.termsOfService.message}</p>
+            )}
          </div>
          <button
             className="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700 cursor-pointer"
