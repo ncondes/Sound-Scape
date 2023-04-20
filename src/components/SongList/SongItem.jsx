c7e2b62aaf0dade81b4a02fd8176e98df681e2c4
nzone56 <jpereirap@unal.edu.co>
2023-04-19 21:55:40 -0500

    Small fixes

--- src/components/SongList/SongItem.jsx
+++ src/components/SongList/SongItem.jsx
@@ -3,10 +3,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export const SongItem = () => {
   return (
-     <div className="text-gray-600 container mx-auto flex justify-between items-center pt-6 pb-2 pl-6 pr-4">
+     <li className="text-gray-600 container mx-auto flex justify-between items-center pt-4 pb-2 pl-6 pr-4 hover:bg-gray-100 cursor-pointer">
         <div>
            <h1 className="font-bold text-lg"> Song Title </h1>
-           <h4 className=""> Artist Name </h4>
+           <h4> Artist Name </h4>
         </div>
         <div>
            <FontAwesomeIcon icon={faHeart} size="lg" />
@@ -14,6 +14,6 @@ export const SongItem = () => {
            <FontAwesomeIcon icon={faComments} size="lg" />
            <span className="font-medium text-lg ml-1"> 5 </span>
         </div>
-     </div>
+     </li>
   )
 }
