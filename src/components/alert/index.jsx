87e3ca993eb39e105a580900163b2e0743c0872e
nzone56 <jpereirap@unal.edu.co>
2023-05-01 21:02:53 -0500

    SS020 Created alertComponent in login/register with uptdate on UserSlice

--- src/components/alert/index.jsx
+++ src/components/alert/index.jsx
@@ -1,10 +1,9 @@
 
 export const Alert = ({alertMessage }) => {
+
     const { message, backgroundColor } = alertMessage
     const boxStyles = `block w-full text-white bg-${backgroundColor} text-center font-semibold p-4 mb-3`
     return (
-       <>
           <div className={boxStyles}>{message}</div>
-       </>
     )
 }
