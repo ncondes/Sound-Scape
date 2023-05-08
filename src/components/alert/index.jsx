3964cd7fac1282ca428b0633f78c3bc64ea8c4fc
nzone56 <jpereirap@unal.edu.co>
2023-05-08 16:27:27 -0500

    Added close button to Alert component

--- src/components/alert/index.jsx
+++ src/components/alert/index.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { useAlertMessage } from '../../hooks'
+
 const variants = {
    success: 'bg-green-500',
    error: 'bg-red-500',
@@ -5,13 +8,30 @@ const variants = {
 }
 
 export const Alert = ({ message, variant }) => {
+   const { setShowAlert, showAlert, displayAlert } = useAlertMessage()
+
+   const handleClose = () => {
+      setShowAlert(false)
+   }
+
+   useEffect(() => {
+      displayAlert()
+   }, [])
+
    return (
-      <div
-         className={`block w-full text-white ${
-            variants[variant] ?? ''
-         } text-center font-semibold p-4 mb-3`}
-      >
-         {message}
-      </div>
+      <>
+         {showAlert ? (
+            <div
+               className={`relative container block w-full text-white ${
+                  variants[variant] ?? ''
+               }  font-semibold mb-3`}
+            >
+               <div className="modal-close cursor-pointer absolute right-0 px-1">
+                  <i className="fas fa-times green" onClick={handleClose}></i>
+               </div>
+               <p className="text-center p-4">{message}</p>
+            </div>
+         ) : null}
+      </>
    )
 }
