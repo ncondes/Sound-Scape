651617457dd32f6ef4faadd37c92a58c40f4b585
nicolas conde <ncondes@unal.edu.co>
2023-05-14 23:14:59 -0500

    move router outside of the component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { HomePage, ManagePage } from '../pages'
 
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <HomePage />
+   },
+   {
+      path: '/manage',
+      element: <ManagePage />
+   }
+])
+
 export const AppRouter = () => {
-   const router = createBrowserRouter([
-      {
-         path: '/',
-         element: <HomePage />
-      },
-      {
-         path: '/manage',
-         element: <ManagePage />
-      }
-   ])
    return <RouterProvider router={router}></RouterProvider>
 }
