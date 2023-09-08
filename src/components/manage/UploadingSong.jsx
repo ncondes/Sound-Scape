38598acb299b709a79bbe6e92aac8d0332f468e2
nicolas conde <ncondes@unal.edu.co>
2023-09-07 20:52:45 -0500

    add text color to the file uploaded

--- src/components/manage/UploadingSong.jsx
+++ src/components/manage/UploadingSong.jsx
@@ -12,16 +12,15 @@ const bgVariants = {
 
 export const UploadingSong = ({ upload }) => {
   const { name, currentProgress, variant, icon } = upload
+  const textColor = textVariants[variant] ?? ''
+  const bgColor = bgVariants[variant] ?? ''
 
   return (
     <div className="mb-4">
-      <i className={`${icon} fa-l ${textVariants[variant] ?? ''} `} />
-      <label className="font-bold text-sm"> {name} </label>
+      <i className={`${icon} fa-l ${textColor} `} />
+      <span className={`font-bold text-sm ml-2 ${textColor}`}>{name}</span>
       <div className="flex rounded overflow-hidden bg-gray-200 h-4">
-        <div
-          className={`transition-all progress-bar ${bgVariants[variant] ?? ''}`}
-          style={{ width: `${currentProgress}%` }}
-        />
+        <div className={`transition-all progress-bar ${bgColor}`} style={{ width: `${currentProgress}%` }} />
       </div>
     </div>
   )
