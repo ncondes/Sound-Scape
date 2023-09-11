4d5b6780d8adb480400ee46edfc4de0eed9a735f
Julian Pereira <jpereirap@unal.edu.co>
2023-09-11 17:38:23 -0500

    ref from songs fixed

--- src/store/uploads/uploads.provider.js
+++ src/store/uploads/uploads.provider.js
@@ -1,9 +1,11 @@
 import { storage } from '@/firebase'
 import { ref, uploadBytesResumable } from 'firebase/storage'
+import { buildSongRef } from '../../utils'
 
 export const startUploadingSong = (file) => {
   // create a reference to the file in the storage bucket
-  const songsRef = ref(storage, `songs/${file.name}`)
+  const songRef = buildSongRef(file.name)
+  const songsRef = ref(storage, `songs/${songRef}`)
   const task = uploadBytesResumable(songsRef, file)
   return task
 }
