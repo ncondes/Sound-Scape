e0f2a15e8eaca47ef356aecf2da4b9a338fb467c
nicolas conde <ncondes@unal.edu.co>
2023-09-12 11:41:04 -0500

    move assests to public dir and add _redirects file

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,32 +1,23 @@
-import background from '../../assets/img/not-found-bg.jpg'
-import record from '../../assets/img/not-found-broken-record.png'
+import background from '/assets/img/not-found-bg.jpg'
+import record from '/assets/img/not-found-broken-record.png'
 
 export const NotFoundPage = () => {
   return (
     <div
-      className='flex flex-col justify-center items-center bg-center bg-cover bg-no-repeat'
-         // 100vh - 72px (header height)
+      className="flex flex-col justify-center items-center bg-center bg-cover bg-no-repeat"
+      // 100vh - 72px (header height)
       style={{ height: 'calc(100vh - 72px)', backgroundImage: `url(${background})` }}
     >
-      <div className='flex justify-center items-center'>
-        <span className='text-[100px] leading-[100px] md:text-[200px] md:leading-[200px] text-amber-500'>
-          4
-        </span>
+      <div className="flex justify-center items-center">
+        <span className="text-[100px] leading-[100px] md:text-[200px] md:leading-[200px] text-amber-500">4</span>
         <div
-          className='h-[100px] w-[100px] md:h-[200px] md:w-[200px]'
+          className="h-[100px] w-[100px] md:h-[200px] md:w-[200px]"
           style={{ backgroundImage: `url(${record})`, backgroundSize: 'cover' }}
         />
-        <span className='text-[100px] leading-[100px] md:text-[200px] md:leading-[200px] text-amber-500'>
-          4
-        </span>
+        <span className="text-[100px] leading-[100px] md:text-[200px] md:leading-[200px] text-amber-500">4</span>
       </div>
-      <h2 className='text-2xl md:text-5xl text-gray-700 my-8 md:my-12'>
-        Sorry, page not found!
-      </h2>
-      <a
-        className='bg-gray-700 mb-10 text-white py-2 px-10 text-lg rounded transition hover:text-amber-500'
-        href='/'
-      >
+      <h2 className="text-2xl md:text-5xl text-gray-700 my-8 md:my-12">Sorry, page not found!</h2>
+      <a className="bg-gray-700 mb-10 text-white py-2 px-10 text-lg rounded transition hover:text-amber-500" href="/">
         Go Home
       </a>
     </div>
